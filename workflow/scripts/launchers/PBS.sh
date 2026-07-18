feb46441243b64b13714381ce7a4d0c9c3b704cb
FilippoGas <filippo.gastaldell@gmail.com>
2026-07-18 11:31:12 +0200

    Added script to launch workflow on PBS managed hpc

---
+++ workflow/scripts/launchers/PBS.sh
@@ -0,0 +1,14 @@
+# Create logdir for PBS inside the repo's results directory
+PBS_LOGDIR="path/for/PBS/logs"
+if [ ! -e $PBS_LOGDIR ]; then
+    mkdir -p $PBS_LOGDIR
+fi
+
+snakemake \
+    --executor cluster-generic \
+    --cluster-generic-submit-cmd "qsub -e $PBS_LOGDIR -o $PBS_LOGDIR -l select=1:ncpus={threads}:mem={resources.mem_mb}mb -l walltime={resources.time} -q {resources.queue}" \
+    --cluster-generic-status-cmd "python workflow/scripts/cluster_status/PBS_status.py" \
+    --jobs 50 \
+    --keep-going \
+    --use-conda \
+    --latency-wait 120
