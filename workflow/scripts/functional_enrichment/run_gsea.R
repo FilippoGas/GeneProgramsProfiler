b8675a2160d14e2535d4c8193a752294183f801a
FilippoGas <filippo.gastaldell@gmail.com>
2026-08-04 16:23:52 +0200

    Added fgsea import

--- workflow/scripts/functional_enrichment/run_gsea.R
+++ workflow/scripts/functional_enrichment/run_gsea.R
@@ -37,6 +37,7 @@ suppressPackageStartupMessages({
         library(patchwork)
         library(msigdbr)
         library(jsonlite)
+        library(fgsea)
 })
 
 message("Starting R script \"run_gsea.R\"...")
@@ -45,7 +46,7 @@ message("Starting R script \"run_gsea.R\"...")
 # ------------------------------------------------------------------------------
 message("Loading results from differential analysis from ",
         snakemake@input[["DEGs_both"]])
-DEGs_sc <- read_csv(snakemake@input[["DEGs_both"]])
+DEGs_both <- read_csv(snakemake@input[["DEGs_both"]])
 message("Loading cytopus gene sets from ",
         snakemake@input[["cytopus"]])
 cytopus <- read_json(snakemake@input[["cytopus"]])
@@ -85,7 +86,7 @@ analysis_configs <- list(
         )
 )
 # For each celltype run gsea for sc and pb DEGs 
-for (cell_type in unique(total_DEGs$celltype)) {
+for (cell_type in unique(DEGs_both$celltype)) {
         # Iterate over sc and pb analyses
         for (analysis_name in names(analysis_configs)) {
                 
@@ -93,7 +94,7 @@ for (cell_type in unique(total_DEGs$celltype)) {
                 cfg <- analysis_configs[[analysis_name]]
                 
                 # Create rank for this celltype based on the configuration
-                celltype_DEGs <- total_DEGs %>% 
+                celltype_DEGs <- DEGs_both %>% 
                         dplyr::filter(celltype == cell_type,
                                       !is.na(.data[[cfg$sig_col]])) %>% 
                         dplyr::arrange(desc(.data[[cfg$rank_col]]))
@@ -101,56 +102,54 @@ for (cell_type in unique(total_DEGs$celltype)) {
                 rank        <- celltype_DEGs[[cfg$rank_col]]
                 names(rank) <- celltype_DEGs$gene
                 rank        <- rank[!is.na(rank)]
-                for (gene_set in gene_sets_names) {
-                        set.seed(42)
-                        fgsea_results <- fgsea(pathways = gene_sets[[gene_set]], 
-                                               stats    = rank,
-                                               minSize  = 5,
-                                               maxSize  = 500)
+                set.seed(42)
+                fgsea_results <- fgsea(pathways = cytopus_list, 
+                                       stats    = rank,
+                                       minSize  = 5,
+                                       maxSize  = 500)
+                
+                sig_results   <- fgsea_results[padj < padj_thresh]
+                top_up        <- head(sig_results[order(-NES)]$pathway, 10)
+                top_down      <- head(sig_results[order(NES)]$pathway, 10)
+                top_pathways  <- unique(c(top_up, top_down))
+                
+                if (!is_empty(top_pathways)) {
+                        p <- as.ggplot(plotGseaTable(gene_sets[[gene_set]][top_pathways],
+                                                     rank, 
+                                                     fgsea_results,
+                                                     gseaParam = 0.5)) +
+                                labs(title = paste0("GSEA of DEGs according to ",
+                                                    cfg$title_desc,
+                                                    " for ",
+                                                    cell_type,
+                                                    " cells using ",
+                                                    gene_set,
+                                                    " gene set."))
                         
-                        sig_results   <- fgsea_results[padj < padj_thresh]
-                        top_up        <- head(sig_results[order(-NES)]$pathway, 10)
-                        top_down      <- head(sig_results[order(NES)]$pathway, 10)
-                        top_pathways  <- unique(c(top_up, top_down))
+                        # Define output directory and filename prefix
+                        out_dir     <- paste0(out_dir, cfg$out_folder, "/", gene_set, "/")
+                        file_prefix <- paste0(out_dir, gsub("/", "_", cell_type))
                         
-                        if (!is_empty(top_pathways)) {
-                                p <- as.ggplot(plotGseaTable(gene_sets[[gene_set]][top_pathways],
-                                                             rank, 
-                                                             fgsea_results,
-                                                             gseaParam = 0.5)) +
-                                        labs(title = paste0("GSEA of DEGs according to ",
-                                                            cfg$title_desc,
-                                                            " for ",
-                                                            cell_type,
-                                                            " cells using ",
-                                                            gene_set,
-                                                            " gene set."))
-                                
-                                # Define output directory and filename prefix
-                                out_dir     <- paste0(out_dir, cfg$out_folder, "/", gene_set, "/")
-                                file_prefix <- paste0(out_dir, gsub("/", "_", cell_type))
-                                
-                                ggsave(p, filename = paste0(file_prefix, ".pdf"), device = "pdf", width = 12, height = 8, create.dir = TRUE)
-                                write_csv(sig_results, file = paste0(file_prefix, ".csv"))
-                                
-                                # clusterProfiler-style dotplot
-                                plot_data <- sig_results[pathway %in% top_pathways]
-                                
-                                # Convert pathway to a factor ordered by NES
-                                plot_data$pathway <- factor(plot_data$pathway, levels = plot_data$pathway[order(plot_data$NES)])
-                                
-                                p_cp_mimic <- ggplot(plot_data, aes(x = NES, y = pathway, color = padj, size = size)) +
-                                        geom_point() +
-                                        scale_color_gradient(low = "red", high = "blue", name = "p.adjust", guide = guide_colorbar(reverse = TRUE)) +
-                                        scale_size_continuous(range = c(3, 8), name = "Set Size") +
-                                        theme_minimal() +
-                                        labs(title = paste0("GSEA of DEGs according to ", cfg$title_desc, "\nfor ", cell_type, " cells using ", gene_set, " gene set."),
-                                             x = "Normalized Enrichment Score (NES)",
-                                             y = NULL) +
-                                        theme(axis.text.y = element_text(size = 10))
-                                
-                                ggsave(p_cp_mimic, filename = paste0(file_prefix, "_dotplot.pdf"), device = "pdf", width = 12, height = 8, create.dir = TRUE)
-                        }
+                        ggsave(p, filename = paste0(file_prefix, ".pdf"), device = "pdf", width = 12, height = 8, create.dir = TRUE)
+                        write_csv(sig_results, file = paste0(file_prefix, ".csv"))
+                        
+                        # clusterProfiler-style dotplot
+                        plot_data <- sig_results[pathway %in% top_pathways]
+                        
+                        # Convert pathway to a factor ordered by NES
+                        plot_data$pathway <- factor(plot_data$pathway, levels = plot_data$pathway[order(plot_data$NES)])
+                        
+                        p_cp_mimic <- ggplot(plot_data, aes(x = NES, y = pathway, color = padj, size = size)) +
+                                geom_point() +
+                                scale_color_gradient(low = "red", high = "blue", name = "p.adjust", guide = guide_colorbar(reverse = TRUE)) +
+                                scale_size_continuous(range = c(3, 8), name = "Set Size") +
+                                theme_minimal() +
+                                labs(title = paste0("GSEA of DEGs according to ", cfg$title_desc, "\nfor ", cell_type, " cells using ", gene_set, " gene set."),
+                                     x = "Normalized Enrichment Score (NES)",
+                                     y = NULL) +
+                                theme(axis.text.y = element_text(size = 10))
+                        
+                        ggsave(p_cp_mimic, filename = paste0(file_prefix, "_dotplot.pdf"), device = "pdf", width = 12, height = 8, create.dir = TRUE)
                 }
         }
 }
